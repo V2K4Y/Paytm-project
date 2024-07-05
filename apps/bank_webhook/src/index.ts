71fea2c5619deff37420986bc7eb5a6c1717a084
vivek <vivek24yadukul@gmail.com>
2024-07-05 12:38:05 +0530

    transaction_1

---
+++ apps/bank_webhook/src/index.ts
@@ -0,0 +1,54 @@
+import express from 'express'
+import db from "@repo/db/client"
+
+const app = express();
+
+app.post('/hdfcWebhook', async (req, res) => { 
+    // TODO: Add zod validation here!
+    type PaymentInformation = {
+        token: string
+        userId: string
+        amount: string
+    }
+    const paymentInformation:PaymentInformation = {
+        token: req.body.token,
+        userId: req.body.user_identifier,
+        amount: req.body.amount
+    }
+
+    try {
+        await db.$transaction([
+            db.balance.update({
+                where: {
+                    userId: Number(paymentInformation.userId)
+                },
+                data: {
+                    amount: {
+                        increment: Number(paymentInformation.amount)
+                    }
+                }
+            }),
+        
+            db.onRampTransaction.update({
+                where: {
+                    token: paymentInformation.token
+                },
+                data: {
+                    status: "Success"
+                }
+            })
+        ])
+
+        res.status(200).json({
+            message: "captured"
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(411).json({
+            message: "Error while processing webhook",
+        })
+    }
+})
+
+app.listen(3005, () => (console.log("Listening on PORT: 3005")))
